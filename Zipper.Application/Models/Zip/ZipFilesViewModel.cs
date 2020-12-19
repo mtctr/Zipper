4029986f6765f38ac9979b0a0d5e53ed390092af
Mateus <mateustargino95@gmail.com>
2020-12-18 23:07:26 -0300

    Add validation of file lenght and count

--- Zipper.Application/Models/Zip/ZipFilesViewModel.cs
+++ Zipper.Application/Models/Zip/ZipFilesViewModel.cs
@@ -1,17 +1,28 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace Zipper.Application.Models.Zip
 {
-    public class ZipFilesViewModel
+    public class ZipFilesViewModel : IValidatableObject
     {
         [DisplayName("Select Files:")]
         public IList<IFormFile> Files { get; set; }
 
         public ZipFilesViewModel()
         {
-            Files = new List<IFormFile>();            
+            Files = new List<IFormFile>();
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Files.Any(x => x.Length > 3e6))
+                yield return new ValidationResult("Files must have less than 3MB.", new string[] { "Files" });
+
+            if(Files.Count > 5)
+                yield return new ValidationResult("You can upload a maximum of 5 files.", new string[] { "Files" });
         }
     }
 }
