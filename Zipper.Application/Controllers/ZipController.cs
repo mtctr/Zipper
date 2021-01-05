fbd353af07810a37ba03fe79e628dc6629508f1d
Mateus <mateustargino95@gmail.com>
2021-01-05 16:47:37 -0300

    Zipper's zipping

--- Zipper.Application/Controllers/ZipController.cs
+++ Zipper.Application/Controllers/ZipController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net.Mime;
 using Zipper.Application.Models.Zip;
 
 namespace Zipper.Application.Controllers
@@ -14,7 +18,32 @@ namespace Zipper.Application.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Index(ZipFilesViewModel viewModel)
         {
-            return View();
+            if (!ModelState.IsValid)
+                return View(viewModel);
+
+            byte[] zipFile;
+            var zipStream = new MemoryStream();
+            zipStream.Seek(0, SeekOrigin.Begin);
+
+            using (ZipArchive zip = new (zipStream, ZipArchiveMode.Create, true))
+            {
+                foreach (var file in viewModel.Files)
+                {
+                    var entry = zip.CreateEntry($"{file.FileName}");
+                    using(Stream entryStream = entry.Open())
+                    {
+                        file.CopyTo(entryStream);
+                    }
+                }
+            }
+
+            zipFile = zipStream.ToArray();
+            zipStream.Flush();
+
+            var responseType = MediaTypeNames.Application.Zip;
+            var fileName = $"Zipper_{Guid.NewGuid()}.zip";
+            Response.Headers.Add("Content-Disposition", $"attachment; filename={fileName}");
+            return File(zipFile, responseType);
         }
     }
 }
